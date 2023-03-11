613403247fcb47a611d35ffc1499a1a96a782754
Rikikesh <adarshamishra98@gmail.com>
2023-03-11 20:12:59 +0545

    Third commit

---
+++ list programs/searching_in_list.py
@@ -0,0 +1,13 @@
+#to check the element is present in the list or not
+def creating_list(values):
+    n=int(input("Enter the number of elements in the list:"))
+    print("\nEnter the elements in list")
+    for item in range(0,n):
+        item=int(input())
+        values.append(item)
+    print(f"The values in the list are: \n {values}")
+def 
+odd_numbers=[]
+creating_list(odd_numbers)
+
+        
