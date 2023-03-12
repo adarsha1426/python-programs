b04891e9622396b3123d17dff8e712bd09ea975f
Rikikesh <adarshamishra98@gmail.com>
2023-03-12 20:04:39 +0545

    Corrected the code searching_in_list

--- list programs/searching_in_list.py
+++ list programs/searching_in_list.py
@@ -1,13 +1,26 @@
 #to check the element is present in the list or not
-def creating_list(values):
+def creating_list():
     n=int(input("Enter the number of elements in the list:"))
+    values=[]
     print("\nEnter the elements in list")
     for item in range(0,n):
         item=int(input())
         values.append(item)
     print(f"The values in the list are: \n {values}")
-def 
-odd_numbers=[]
-creating_list(odd_numbers)
+    return values
 
-        
+#searching through the list
+def search():
+    list_1=creating_list()
+    n=len(list_1)
+    search_item=int(input("Enter the value to be searched:\n"))
+    found=0
+    for item in list_1:
+            if search_item==item:
+              found=1
+              break
+    if found:
+         print("Element is Found")
+    elif found==1:
+         print("Not found")
+search()
